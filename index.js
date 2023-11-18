b98f7fe2cf3242dbca3b94a00fb1fc037a5daf6e
Oleg <nn-kms@yandex.ru>
2023-11-18 13:07:21 +0300

    1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 var slider = {
-    slides:['img/hero-slider/hero-1-1140px.svg','img/hero-slider/hero-2-1140px.svg', 'img/hero-slider/hero-3-1140px.svg'],
+    slides:['img/oleg.jpg','img/hero-slider/hero-2-1140px.svg', 'img/hero-slider/hero-3-1140px.svg'],
     frame:0,
     set: function(image) {
         document.getElementById("src").style.backgroundImage = "url("+image+")";
